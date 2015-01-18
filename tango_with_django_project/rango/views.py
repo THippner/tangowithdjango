5f3fd5c1f632881cea4970fdd0ac7f82bc582c08
THippner <icelore@gmail.com>
2015-01-18 16:50:00 +0000

    Chapter 4 - Complete, really this time.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Rango says hey there world")
+
+def about(request):
+	return HttpResponse("<HTML><HEAD></HEAD><BODY>Rango says here is the about page.<BR><A href=\"/rango/\">Back to /rango</A></BODY></HTML>")
